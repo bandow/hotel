a0e90eaba381dd16b383dcd3eb2a52196add6800
goSnowbank <sycbbb@sina.com>
2016-09-27 22:33:57 +0800

    hotel
    hotel

--- public/scripts/orders.js
+++ public/scripts/orders.js
@@ -6,82 +6,9 @@ requirejs.config({
 
 requirejs(['jquery','common'],function($,common){  
   $(document).ready(function () {
-    //orders-submit
-    
-
-    //orders-report
-    $(".profit-list dl dd:even").css("backgroundColor","#f4f4f4");
-    $(".profit-list dl dd:odd").css("backgroundColor","#fff");
-
-    $(".profit-list .radio-text").on("click",function(){
-        $(this).next().show();
-    });
-    $(".confirm").on("click",function(){
-       $(this).parent().parent().hide();
-    });
-    $(".container").children().not(".profit-list").on("click",function(){
-       $(".profit-list dl").hide();
-    });
-
-    $(".profit-list dd").on("click",function(){
-      $(this).addClass("active").siblings().removeClass("active");
-      $("#radioTxte").html($(this).html());
-      $(".profit-list dl").hide();
-    });
-    
-    $(".profit-surface ul li").on("click",function(){
-       $(this).addClass("active").siblings().removeClass("active");
-       $('.profit-surface-all .profit-surface').eq($(this).index()).show().siblings().hide();
-       var indexNum=$('.profit-surface-all .profit-surface').eq($(this).index()).attr("data-index");
-        if(indexNum==1){
-          $(".profit-surface ul").css({
-            transform:"translate(60vw,0)"
-          })
-        }else if(indexNum==2){
-          $(".profit-surface ul").css({
-            transform:"translate(60vw,0)"
-          })
-        }else if(indexNum==3){
-          $(".profit-surface ul").css({
-            transform:"translate(60vw,0)"
-          })
-        }else if(indexNum==4){
-          $(".profit-surface ul").css({
-            transform:"translate(40vw,0)"
-          })
-        }else if(indexNum==5){
-          $(".profit-surface ul").css({
-            transform:"translate(20vw,0)"
-          })
-        }else if(indexNum==6){
-          $(".profit-surface ul").css({
-            transform:"translate(0,0)"
-          })
-        }else if(indexNum==7){
-          $(".profit-surface ul").css({
-            transform:"translate(-20vw,0)"
-          })
-       }else if(indexNum==8){
-          $(".profit-surface ul").css({
-            transform:"translate(-40vw,0)"
-          })
-       }else if(indexNum==9){
-          $(".profit-surface ul").css({
-            transform:"translate(-60vw,0)"
-          })
-       }else if(indexNum==10){
-          $(".profit-surface ul").css({
-            transform:"translate(-80vw,0)"
-          })
-       }else if(indexNum==11){
-          $(".profit-surface ul").css({
-            transform:"translate(-80vw,0)"
-          })
-       }else if(indexNum==12){
-          $(".profit-surface ul").css({
-            transform:"translate(-80vw,0)"
-          })
-       }
+    //top-contacts
+    $(".head-click").on("click",function(){
+       $(".add-order-check-in").show();
     });
     
   });    
